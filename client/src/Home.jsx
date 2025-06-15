63c2ed4e226f156cd86cdf9ec95610c1d095d9e5
Akashdeep <akashdeep2k4@gmail.com>
2025-06-16 01:21:53 +0530

    add Home component for displaying and managing todo list

---
+++ client/src/Home.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import Create from "./Create";
+import { useEffect } from "react";
+import axios from "axios";
+
+import { BsFillTrashFill, BsCircleFill, BsCheckCircleFill } from "react-icons/bs";
+
+export default function Home() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/get")
+      .then((result) => setTodos(result.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleEdit = (id) => {
+    axios
+      .put("http://localhost:3000/update/" + id)
+      .then((result) => {
+        location.reload();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const handleDelete = (id) => {
+    axios
+      .delete("http://localhost:3000/delete/" + id)
+      .then((result) => {
+        location.reload();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="home">
+      <h2>Todo List</h2>
+      <Create />
+      {todos.length === 0 ? (
+        <div>
+          <h2>No Record</h2>
+        </div>
+      ) : (
+        todos.map((todo) => (
+          <div className="task">
+            <div className="checkbox" onClick={() => handleEdit(todo._id)}>
+              {todo.done ? <BsCheckCircleFill className="icon" /> : <BsCircleFill className="icon" />}
+              <p className={todo.done ? "line-through" : ""}>{todo.task}</p>
+            </div>
+            <div>
+              <span>
+                <BsFillTrashFill className="icon" onClick={() => handleDelete(todo._id)} />
+              </span>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
